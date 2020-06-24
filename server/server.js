58babd406565debf5dd0b419eade207e6f489691
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-06-24 17:49:24 -0500

    revised required

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require('express'),
     path = require('path'),
     app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
 app.use((req, res, next) => {
     console.log(req.originalUrl);
     next();
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3000);
